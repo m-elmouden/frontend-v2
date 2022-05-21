70713e7d90d7a85454664075ad869834520edd91
m-elmouden <m.elmouden589@edu.uca.ma>
2022-05-21 16:24:11 +0100

    (changesByMohamed)

--- src/app/module/admin/view/impot/declaration-ir-employe-admin/create-admin/declaration-ir-employe-create-admin.component.ts
+++ src/app/module/admin/view/impot/declaration-ir-employe-admin/create-admin/declaration-ir-employe-create-admin.component.ts
@@ -1,11 +1,10 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DeclarationIrEmployeService} from '../../../../../../controller/service/DeclarationIrEmploye.service';
 import {DeclarationIrEmployeVo} from '../../../../../../controller/model/DeclarationIrEmploye.model';
 import {RoleService} from '../../../../../../controller/service/role.service';
 import {MessageService} from 'primeng/api';
 import {Router} from '@angular/router';
-import {MenuItem} from 'primeng/api';
-import { environment } from 'src/environments/environment';
+import {environment} from 'src/environments/environment';
 import {DatePipe} from '@angular/common';
 import {StringUtilService} from '../../../../../../controller/service/StringUtil.service';
 
@@ -16,17 +15,18 @@ import {TauxIrVo} from '../../../../../../controller/model/TauxIr.model';
 import {TauxIrService} from '../../../../../../controller/service/TauxIr.service';
 import {EmployeVo} from '../../../../../../controller/model/Employe.model';
 import {EmployeService} from '../../../../../../controller/service/Employe.service';
+
 @Component({
-  selector: 'app-declaration-ir-employe-create-admin',
-  templateUrl: './declaration-ir-employe-create-admin.component.html',
-  styleUrls: ['./declaration-ir-employe-create-admin.component.css']
+    selector: 'app-declaration-ir-employe-create-admin',
+    templateUrl: './declaration-ir-employe-create-admin.component.html',
+    styleUrls: ['./declaration-ir-employe-create-admin.component.css']
 })
 export class DeclarationIrEmployeCreateAdminComponent implements OnInit {
 
     _submitted = false;
     private _errorMessages = new Array<string>();
 
-   _validDeclarationIrEmployeSalaireBase = true;
+    _validDeclarationIrEmployeSalaireBase = true;
 
     _validDeclarationIrRefrerence = true;
     _validDeclarationIrAnnee = true;
@@ -38,71 +38,68 @@ export class DeclarationIrEmployeCreateAdminComponent implements OnInit {
     _validEmployeNombreFamille = true;
 
 
+    constructor(private datePipe: DatePipe, private declarationIrEmployeService: DeclarationIrEmployeService
+        , private stringUtilService: StringUtilService
+        , private roleService: RoleService
+        , private messageService: MessageService
+        , private router: Router
+        , private declarationIrService: DeclarationIrService
+        , private tauxIrService: TauxIrService
+        , private employeService: EmployeService
+    ) {
 
-constructor(private datePipe: DatePipe, private declarationIrEmployeService: DeclarationIrEmployeService
- ,       private stringUtilService: StringUtilService
- ,       private roleService:RoleService
- ,       private messageService: MessageService
- ,       private router: Router
- 
-,       private declarationIrService :DeclarationIrService
-,       private tauxIrService :TauxIrService
-,       private employeService :EmployeService
-) {
-
-}
+    }
 
 
 // methods
-ngOnInit(): void {
+    ngOnInit(): void {
 
-    this.selectedDeclarationIr = new DeclarationIrVo();
-    this.declarationIrService.findAll().subscribe((data) => this.declarationIrs = data);
-    this.selectedEmploye = new EmployeVo();
-    this.employeService.findAll().subscribe((data) => this.employes = data);
-    this.selectedTauxIr = new TauxIrVo();
-    this.tauxIrService.findAll().subscribe((data) => this.tauxIrs = data);
-}
-
-
-
-
-private setValidation(value : boolean){
-    this.validDeclarationIrEmployeSalaireBase = value;
+        this.selectedDeclarationIr = new DeclarationIrVo();
+        this.declarationIrService.findAll().subscribe((data) => this.declarationIrs = data);
+        this.selectedEmploye = new EmployeVo();
+        this.employeService.findAll().subscribe((data) => this.employes = data);
+        this.selectedTauxIr = new TauxIrVo();
+        this.tauxIrService.findAll().subscribe((data) => this.tauxIrs = data);
     }
 
 
-public save(){
-  this.submitted = true;
-  this.validateForm();
-  if (this.errorMessages.length === 0) {
-        this.saveWithShowOption(false);
-  } else {
-        this.messageService.add({severity: 'error', summary: 'Erreurs', detail: 'Merci de corrigé les erreurs sur le formulaire'});
-  }
-}
-
-public saveWithShowOption(showList: boolean){
-     this.declarationIrEmployeService.save().subscribe(declarationIrEmploye=>{
-       this.declarationIrEmployes.push({...declarationIrEmploye});
-       this.createDeclarationIrEmployeDialog = false;
-       this.submitted = false;
-       this.selectedDeclarationIrEmploye = new DeclarationIrEmployeVo();
+    private setValidation(value: boolean) {
+        this.validDeclarationIrEmployeSalaireBase = value;
+    }
 
 
-    } , error =>{
-        console.log(error);
-    });
+    public save() {
+        this.submitted = true;
+        this.validateForm();
+        if (this.errorMessages.length === 0) {
+            this.saveWithShowOption(false);
+        } else {
+            this.messageService.add({severity: 'error', summary: 'Erreurs', detail: 'Merci de corrigé les erreurs sur le formulaire'});
+        }
+    }
 
-}
+    public saveWithShowOption(showList: boolean) {
+        this.declarationIrEmployeService.save().subscribe(declarationIrEmploye => {
+            this.declarationIrEmployes.push({...declarationIrEmploye});
+            this.createDeclarationIrEmployeDialog = false;
+            this.submitted = false;
+            this.selectedDeclarationIrEmploye = new DeclarationIrEmployeVo();
+
+
+        }, error => {
+            console.log(error);
+        });
+
+    }
+
 //validation methods
-private validateForm(): void{
-this.errorMessages = new Array<string>();
-this.validateDeclarationIrEmployeSalaireBase();
+    private validateForm(): void {
+        this.errorMessages = new Array<string>();
+        this.validateDeclarationIrEmployeSalaireBase();
 
     }
 
-private validateDeclarationIrEmployeSalaireBase(){
+    private validateDeclarationIrEmployeSalaireBase() {
         if (this.stringUtilService.isEmpty(this.selectedDeclarationIrEmploye.salaireBase)) {
             this.errorMessages.push('Salaire base non valide');
             this.validDeclarationIrEmployeSalaireBase = false;
@@ -112,149 +109,158 @@ private validateDeclarationIrEmployeSalaireBase(){
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //openPopup
-              public async openCreatedeclarationIr(declarationIr: string) {
-                      const isPermistted = await this.roleService.isPermitted('DeclarationIr', 'add');
-                       if(isPermistted){
-         this.selectedDeclarationIr = new DeclarationIrVo();
-        this.createDeclarationIrDialog = true;
-        }else{
-             this.messageService.add({
+    public async openCreatedeclarationIr(declarationIr: string) {
+        const isPermistted = await this.roleService.isPermitted('DeclarationIr', 'add');
+        if (isPermistted) {
+            this.selectedDeclarationIr = new DeclarationIrVo();
+            this.createDeclarationIrDialog = true;
+        } else {
+            this.messageService.add({
                 severity: 'error', summary: 'erreur', detail: 'problème de permission'
             });
         }
-}
-              public async openCreatetauxIr(tauxIr: string) {
-                      const isPermistted = await this.roleService.isPermitted('TauxIr', 'add');
-                       if(isPermistted){
-         this.selectedTauxIr = new TauxIrVo();
-        this.createTauxIrDialog = true;
-        }else{
-             this.messageService.add({
+    }
+
+    public async openCreatetauxIr(tauxIr: string) {
+        const isPermistted = await this.roleService.isPermitted('TauxIr', 'add');
+        if (isPermistted) {
+            this.selectedTauxIr = new TauxIrVo();
+            this.createTauxIrDialog = true;
+        } else {
+            this.messageService.add({
                 severity: 'error', summary: 'erreur', detail: 'problème de permission'
             });
         }
-}
-              public async openCreateemploye(employe: string) {
-                      const isPermistted = await this.roleService.isPermitted('Employe', 'add');
-                       if(isPermistted){
-         this.selectedEmploye = new EmployeVo();
-        this.createEmployeDialog = true;
-        }else{
-             this.messageService.add({
+    }
+
+    public async openCreateemploye(employe: string) {
+        const isPermistted = await this.roleService.isPermitted('Employe', 'add');
+        if (isPermistted) {
+            this.selectedEmploye = new EmployeVo();
+            this.createEmployeDialog = true;
+        } else {
+            this.messageService.add({
                 severity: 'error', summary: 'erreur', detail: 'problème de permission'
             });
         }
-}
+    }
+
 // methods
 
-hideCreateDialog(){
-    this.createDeclarationIrEmployeDialog  = false;
-    this.setValidation(true);
-}
+    hideCreateDialog() {
+        this.createDeclarationIrEmployeDialog = false;
+        this.setValidation(true);
+    }
 
 // getters and setters
 
-get declarationIrEmployes(): Array<DeclarationIrEmployeVo> {
-    return this.declarationIrEmployeService.declarationIrEmployes;
-       }
-set declarationIrEmployes(value: Array<DeclarationIrEmployeVo>) {
-        this.declarationIrEmployeService.declarationIrEmployes = value;
-       }
-
- get selectedDeclarationIrEmploye():DeclarationIrEmployeVo {
-           return this.declarationIrEmployeService.selectedDeclarationIrEmploye;
-       }
-    set selectedDeclarationIrEmploye(value: DeclarationIrEmployeVo) {
-        this.declarationIrEmployeService.selectedDeclarationIrEmploye = value;
-       }
-
-   get createDeclarationIrEmployeDialog(): boolean {
-           return this.declarationIrEmployeService.createDeclarationIrEmployeDialog;
-
-       }
-    set createDeclarationIrEmployeDialog(value: boolean) {
-        this.declarationIrEmployeService.createDeclarationIrEmployeDialog= value;
-       }
-
-       get selectedDeclarationIr(): DeclarationIrVo {
-           return this.declarationIrService.selectedDeclarationIr;
-       }
-      set selectedDeclarationIr(value: DeclarationIrVo) {
-        this.declarationIrService.selectedDeclarationIr = value;
-       }
-       get declarationIrs(): Array<DeclarationIrVo> {
-           return this.declarationIrService.declarationIrs;
-       }
-       set declarationIrs(value: Array<DeclarationIrVo>) {
-        this.declarationIrService.declarationIrs = value;
-       }
-       get createDeclarationIrDialog(): boolean {
-           return this.declarationIrService.createDeclarationIrDialog;
-       }
-      set createDeclarationIrDialog(value: boolean) {
-        this.declarationIrService.createDeclarationIrDialog= value;
-       }
-       get selectedTauxIr(): TauxIrVo {
-           return this.tauxIrService.selectedTauxIr;
-       }
-      set selectedTauxIr(value: TauxIrVo) {
-        this.tauxIrService.selectedTauxIr = value;
-       }
-       get tauxIrs(): Array<TauxIrVo> {
-           return this.tauxIrService.tauxIrs;
-       }
-       set tauxIrs(value: Array<TauxIrVo>) {
-        this.tauxIrService.tauxIrs = value;
-       }
-       get createTauxIrDialog(): boolean {
-           return this.tauxIrService.createTauxIrDialog;
-       }
-      set createTauxIrDialog(value: boolean) {
-        this.tauxIrService.createTauxIrDialog= value;
-       }
-       get selectedEmploye(): EmployeVo {
-           return this.employeService.selectedEmploye;
-       }
-      set selectedEmploye(value: EmployeVo) {
-        this.employeService.selectedEmploye = value;
-       }
-       get employes(): Array<EmployeVo> {
-           return this.employeService.employes;
-       }
-       set employes(value: Array<EmployeVo>) {
-        this.employeService.employes = value;
-       }
-       get createEmployeDialog(): boolean {
-           return this.employeService.createEmployeDialog;
-       }
-      set createEmployeDialog(value: boolean) {
-        this.employeService.createEmployeDialog= value;
-       }
-
-    get dateFormat(){
-            return environment.dateFormatCreate;
+    get declarationIrEmployes(): Array<DeclarationIrEmployeVo> {
+        return this.declarationIrEmployeService.declarationIrEmployes;
     }
 
-    get dateFormatColumn(){
-            return environment.dateFormatList;
-     }
+    set declarationIrEmployes(value: Array<DeclarationIrEmployeVo>) {
+        this.declarationIrEmployeService.declarationIrEmployes = value;
+    }
 
-     get submitted(): boolean {
+    get selectedDeclarationIrEmploye(): DeclarationIrEmployeVo {
+        return this.declarationIrEmployeService.selectedDeclarationIrEmploye;
+    }
+
+    set selectedDeclarationIrEmploye(value: DeclarationIrEmployeVo) {
+        this.declarationIrEmployeService.selectedDeclarationIrEmploye = value;
+    }
+
+    get createDeclarationIrEmployeDialog(): boolean {
+        return this.declarationIrEmployeService.createDeclarationIrEmployeDialog;
+
+    }
+
+    set createDeclarationIrEmployeDialog(value: boolean) {
+        this.declarationIrEmployeService.createDeclarationIrEmployeDialog = value;
+    }
+
+    get selectedDeclarationIr(): DeclarationIrVo {
+        return this.declarationIrService.selectedDeclarationIr;
+    }
+
+    set selectedDeclarationIr(value: DeclarationIrVo) {
+        this.declarationIrService.selectedDeclarationIr = value;
+    }
+
+    get declarationIrs(): Array<DeclarationIrVo> {
+        return this.declarationIrService.declarationIrs;
+    }
+
+    set declarationIrs(value: Array<DeclarationIrVo>) {
+        this.declarationIrService.declarationIrs = value;
+    }
+
+    get createDeclarationIrDialog(): boolean {
+        return this.declarationIrService.createDeclarationIrDialog;
+    }
+
+    set createDeclarationIrDialog(value: boolean) {
+        this.declarationIrService.createDeclarationIrDialog = value;
+    }
+
+    get selectedTauxIr(): TauxIrVo {
+        return this.tauxIrService.selectedTauxIr;
+    }
+
+    set selectedTauxIr(value: TauxIrVo) {
+        this.tauxIrService.selectedTauxIr = value;
+    }
+
+    get tauxIrs(): Array<TauxIrVo> {
+        return this.tauxIrService.tauxIrs;
+    }
+
+    set tauxIrs(value: Array<TauxIrVo>) {
+        this.tauxIrService.tauxIrs = value;
+    }
+
+    get createTauxIrDialog(): boolean {
+        return this.tauxIrService.createTauxIrDialog;
+    }
+
+    set createTauxIrDialog(value: boolean) {
+        this.tauxIrService.createTauxIrDialog = value;
+    }
+
+    get selectedEmploye(): EmployeVo {
+        return this.employeService.selectedEmploye;
+    }
+
+    set selectedEmploye(value: EmployeVo) {
+        this.employeService.selectedEmploye = value;
+    }
+
+    get employes(): Array<EmployeVo> {
+        return this.employeService.employes;
+    }
+
+    set employes(value: Array<EmployeVo>) {
+        this.employeService.employes = value;
+    }
+
+    get createEmployeDialog(): boolean {
+        return this.employeService.createEmployeDialog;
+    }
+
+    set createEmployeDialog(value: boolean) {
+        this.employeService.createEmployeDialog = value;
+    }
+
+    get dateFormat() {
+        return environment.dateFormatCreate;
+    }
+
+    get dateFormatColumn() {
+        return environment.dateFormatList;
+    }
+
+    get submitted(): boolean {
         return this._submitted;
     }
 
@@ -263,79 +269,84 @@ set declarationIrEmployes(value: Array<DeclarationIrEmployeVo>) {
     }
 
 
-
-
     get errorMessages(): string[] {
-    return this._errorMessages;
+        return this._errorMessages;
     }
 
     set errorMessages(value: string[]) {
-    this._errorMessages = value;
+        this._errorMessages = value;
     }
 
     get validDeclarationIrEmployeSalaireBase(): boolean {
-    return this._validDeclarationIrEmployeSalaireBase;
+        return this._validDeclarationIrEmployeSalaireBase;
     }
 
     set validDeclarationIrEmployeSalaireBase(value: boolean) {
-    this._validDeclarationIrEmployeSalaireBase = value;
+        this._validDeclarationIrEmployeSalaireBase = value;
     }
 
     get validDeclarationIrRefrerence(): boolean {
-    return this._validDeclarationIrRefrerence;
+        return this._validDeclarationIrRefrerence;
     }
 
     set validDeclarationIrRefrerence(value: boolean) {
-    this._validDeclarationIrRefrerence = value;
+        this._validDeclarationIrRefrerence = value;
     }
+
     get validDeclarationIrAnnee(): boolean {
-    return this._validDeclarationIrAnnee;
+        return this._validDeclarationIrAnnee;
     }
 
     set validDeclarationIrAnnee(value: boolean) {
-    this._validDeclarationIrAnnee = value;
+        this._validDeclarationIrAnnee = value;
     }
+
     get validDeclarationIrMois(): boolean {
-    return this._validDeclarationIrMois;
+        return this._validDeclarationIrMois;
     }
 
     set validDeclarationIrMois(value: boolean) {
-    this._validDeclarationIrMois = value;
+        this._validDeclarationIrMois = value;
     }
+
     get validEmployeCin(): boolean {
-    return this._validEmployeCin;
+        return this._validEmployeCin;
     }
 
     set validEmployeCin(value: boolean) {
-    this._validEmployeCin = value;
+        this._validEmployeCin = value;
     }
+
     get validEmployeNom(): boolean {
-    return this._validEmployeNom;
+        return this._validEmployeNom;
     }
 
     set validEmployeNom(value: boolean) {
-    this._validEmployeNom = value;
+        this._validEmployeNom = value;
     }
+
     get validEmployePrenom(): boolean {
-    return this._validEmployePrenom;
+        return this._validEmployePrenom;
     }
 
     set validEmployePrenom(value: boolean) {
-    this._validEmployePrenom = value;
+        this._validEmployePrenom = value;
     }
+
     get validEmployeTotalSalaireNet(): boolean {
-    return this._validEmployeTotalSalaireNet;
+        return this._validEmployeTotalSalaireNet;
     }
 
     set validEmployeTotalSalaireNet(value: boolean) {
-    this._validEmployeTotalSalaireNet = value;
+        this._validEmployeTotalSalaireNet = value;
     }
+
     get validEmployeNombreFamille(): boolean {
-    return this._validEmployeNombreFamille;
+        return this._validEmployeNombreFamille;
     }
 
     set validEmployeNombreFamille(value: boolean) {
-    this._validEmployeNombreFamille = value;
+        this._validEmployeNombreFamille = value;
     }
 
 }
