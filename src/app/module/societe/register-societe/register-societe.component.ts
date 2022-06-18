5c21be10f5fcc0c6ff4e9364c35bcf05e7597ec2
Achraf <kharbabachraf@gmail.com>
2022-06-18 07:34:09 +0100

    Modofied by achraf (comptable and societe chagend)5

--- src/app/module/societe/register-societe/register-societe.component.ts
+++ src/app/module/societe/register-societe/register-societe.component.ts
@@ -1,40 +1,66 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
-import { User } from 'src/app/controller/model/User.model';
-import { AuthService } from 'src/app/controller/service/Auth.service';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormControlName, FormGroup, Validators} from '@angular/forms';
+import {User} from 'src/app/controller/model/User.model';
+import {AuthService} from 'src/app/controller/service/Auth.service';
 import {Role} from '../../../controller/model/Role.model';
+import {SocieteVo} from '../../../controller/model/Societe.model';
+import {SocieteService} from '../../../controller/service/Societe.service';
 
 @Component({
-  selector: 'app-register-societe',
-  templateUrl: './register-societe.component.html',
-  styleUrls: ['./register-societe.component.scss']
+    selector: 'app-register-societe',
+    templateUrl: './register-societe.component.html',
+    styleUrls: ['./register-societe.component.scss']
 })
 export class RegisterSocieteComponent implements OnInit {
-  registerForm = new FormGroup({
-    prenom : new FormControl('',Validators.required),
-    nom : new FormControl('',Validators.required),
-    email:new FormControl('',Validators.required),
-    userName : new FormControl('',Validators.required),
-    password : new FormControl('',Validators.required)
-  })
-  constructor(private authService:AuthService) { }
+    registerForm = new FormGroup({
+        prenom: new FormControl('', Validators.required),
+        nom: new FormControl('', Validators.required),
+        email: new FormControl('', Validators.required),
+        userName: new FormControl('', Validators.required),
+        password: new FormControl('', Validators.required)
+    });
 
-  ngOnInit(): void {
-  }
-  submit(){
-    const formValues = this.registerForm.value;
-    const {prenom,nom,userName,password,email} = formValues;
-    const role = new Role();
-    role.authority = 'ROLE_Societe' ;
-    this.user.prenom = prenom;
-    this.user.nom = nom;
-    this.user.username = userName;
-    this.user.password = password;
-    this.user.email = email;
-    this.user.roles = [role] ;
-    this.authService.registerSociete();
+    constructor(private authService: AuthService, private societeService: SocieteService) {
+    }
 
-  }
+    ngOnInit(): void {
+    }
+
+    submit() {
+        const formValues = this.registerForm.value;
+        const {prenom, nom, userName, password, email} = formValues;
+        const role = new Role();
+        role.authority = 'ROLE_Societe';
+        this.user.prenom = prenom;
+        this.user.nom = nom;
+        this.user.username = userName;
+        this.user.password = password;
+        this.user.email = email;
+        this.user.roles = [role];
+        this.authService.registerSociete();
+
+    }
+
+   /* submit2() {
+        const formValues = this.registerForm.value;
+        const {ice, prenom, nom, userName, password, telephone, fax, email, adresse, age} = formValues;
+        this.selectedSociete.prenom = prenom;
+        this.selectedSociete.nom = nom;
+        this.selectedSociete.username = userName;
+        this.selectedSociete.password = password;
+        this.selectedSociete.email = email;
+        this.selectedSociete.telephone = telephone;
+        this.selectedSociete.adresse = adresse;
+        this.selectedSociete.fax = fax;
+        this.selectedSociete.age = age;
+        this.selectedSociete.ice = ice;
+        // this.selectedAdherent.username2 = userName;
+        // this.selectedAdherent.nomPrenom = nomPrenom;
+        this.authService.registerComptable();
+
+
+    }*/
+
     get user(): User {
         return this.authService.user;
     }
@@ -42,5 +68,14 @@ export class RegisterSocieteComponent implements OnInit {
     set user(value: User) {
         this.authService.user = value;
     }
+
+    /*get selectedSociete(): SocieteVo {
+        return this.societeService.selectedSociete;
+    }
+
+    set selectedSociete(value: SocieteVo) {
+        this.societeService.selectedSociete = value;
+    }*/
+
 
 }
